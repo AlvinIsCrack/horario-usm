fe8567f3eb27e0e63ae18b2b0a8970ac91d366b1
AlvinIsCrack <animations468@gmail.com>
2026-07-05 13:33:00 -0400

    fix(BlockContent): improve conditional rendering for bloques and enhance layout for multiple items

--- src/lib/components/calendar/BlockContent.svelte
+++ src/lib/components/calendar/BlockContent.svelte
@@ -8,14 +8,28 @@
 <div
 	class="bg-background/20 ring-input h-full w-full rounded-lg ring will-change-transform ring-inset"
 >
-	{#if !bloques || bloques.length === 0}{:else if bloques.length === 1}
-		<RamoCard bloqueObject={bloques[0]} />
-	{:else if bloques.length === 2}
-		<div class="flex h-full w-full flex-row gap-1 p-1">
+	{#if bloques}
+		{#if bloques.length === 1}
 			<RamoCard bloqueObject={bloques[0]} />
-			<RamoCard bloqueObject={bloques[1]} />
-		</div>
-	{:else}
-		<div class="h-full w-full p-2">3 RAMOS EN CONFLICTO, ESTO NO ESTÁ IMPLEMENTADO AÚN.</div>
+		{:else if bloques.length === 2}
+			<div class="flex h-full w-full flex-row gap-1 p-1">
+				<RamoCard bloqueObject={bloques[0]} />
+				<RamoCard bloqueObject={bloques[1]} />
+			</div>
+		{:else}
+			<div
+				class="text-foreground from-destructive to-destructive/60 relative flex h-full flex-col gap-0.5 overflow-hidden rounded-lg bg-linear-to-br p-3 text-sm leading-tight font-medium tracking-tight"
+			>
+				<div
+					class="border-destructive-foreground! absolute top-0 left-0 size-full animate-pulse border-4"
+				></div>
+
+				{#each bloques as el}
+					<div class="text-destructive-foreground w-full truncate border-b py-0.5 font-mono">
+						{el.ramo?.sigla} - P{el.ramo?.paralelo}
+					</div>
+				{/each}
+			</div>
+		{/if}
 	{/if}
 </div>
