7f0a057b4534367b2851db3c31ce4e32071f3d68
AlvinIsCrack <animations468@gmail.com>
2026-07-12 17:54:52 -0400

    feat(ramos, icons): added icons for questoinare

---
+++ src/lib/core/ramos/repo/components/icons/ConstantCurve.svelte
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+	<path
+		d="M 3,21 L 3,3 M 3,3 L 1,6 M 3,3 L 5,6 M 3,21 L 21,21 M 21,21 L 18,19 M 21,21 L 18,23 M 3,12 L 21,12"
+		stroke="currentColor"
+		stroke-width="2"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
